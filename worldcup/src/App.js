18d4b7ce72b3cbb93ba0ac2458306bb79fb30e0d
AhmedIz123 <ahmedizral9@gmail.com>
2023-12-15 15:58:39 +0100

    1
    cc

--- worldcup/src/App.js
+++ worldcup/src/App.js
@@ -3,19 +3,20 @@ import Logo from './images/Unity kick 1.png';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Dashboard from './components/pages/dashboard/Dashboard';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 function App() {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1100);
-  const [isNavOpen, setIsNavOpen]= useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1400);
+  const [isNavOpen, setIsNavOpen]= useState(false);
     useEffect(()=>{
-        const handleResize = () => setIsSmallScreen(window.innerWidth < 1100);
+        const handleResize = () => setIsSmallScreen(window.innerWidth < 1400);
         window.addEventListener('resize', handleResize);
         // Clean up the event listener when the component unmounts
         return () => window.removeEventListener('resize', handleResize);
     },[window.innerWidth]);
   return (
-    <div className={`page-content ${window.innerWidth < 800?`block`:`inline`}`}>
-      <div className={`sidebar ${window.innerWidth < 800?`block`:`inline`}`}>
-        {window.innerWidth < 800 && (
+    <div className={`page-content ${isSmallScreen?`block`:`inline`}`}>
+      <div className={`sidebar ${isSmallScreen?`block`:`inline`}`}>
+        {isSmallScreen && (
           <div id='burger' className={`logo-menu`}>
             <i id='burger' class="fa-solid fa-bars fa-xl" style={{color: "#636363;"}} onClick={()=>{setIsNavOpen(!isNavOpen)}}></i>
             <div className="logo">
@@ -27,7 +28,11 @@ function App() {
         <Navbar isSmallScreen={isSmallScreen} isNavOpen={isNavOpen} setNavOpen={setIsNavOpen}/>
       </div>
       <div className='right-side'>
-        <Dashboard/>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+          </Routes>
+        </Router>
       </div>
     </div>
   );
--- worldcup/src/components/navbar/Navbar.js
+++ worldcup/src/components/navbar/Navbar.js
@@ -6,11 +6,11 @@ import Palestine from '../../images/State of Palestine (PS).svg';
 import './Navbar.css';
 export default function Navbar({isSmallScreen,isNavOpen,setNavOpen}){
     return<>
-        <div className={`navbar-content ${isNavOpen?``:`hide-nav`} ${isSmallScreen?`mobile`:``}`}>
+        <div className={`navbar-content ${(isNavOpen==false && isSmallScreen)?`hide-nav`:``} ${isSmallScreen?`mobile`:``}`}>
             <div className="logo">
                 <img src={Logo} alt='Logo'/>
                 <h2>Unity Kick</h2>
-                {window.innerWidth < 800 && (<svg xmlns="http://www.w3.org/2000/svg" height="20" width="15" viewBox="0 0 384 512" onClick={()=>{setNavOpen(!isNavOpen)}}><path fill="#636363" d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>)}
+                {isSmallScreen && (<svg xmlns="http://www.w3.org/2000/svg" height="20" width="15" viewBox="0 0 384 512" onClick={()=>{setNavOpen(!isNavOpen)}}><path fill="#636363" d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>)}
             </div>
             <div className='nav-routes'>
                 <div className='menu'>
